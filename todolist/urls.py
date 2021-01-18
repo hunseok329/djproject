9488d9d7fe9235185641cefe5d028594f3bf5a49
hunseok329 <kha5064@gmail.com>
2021-01-18 17:51:06 +0900

    #add todo_delete function

--- todolist/urls.py
+++ todolist/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.todo_list, name="todo_list"),
+    path('todo_create', views.todo_create, name="todo_create"),
+    path('todo_delete/<int:pk>', views.todo_delete, name="todo_delete")
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import ToDoList
+from django.utils import timezone
 
 
 def todo_list(request):
@@ -8,5 +9,15 @@ def todo_list(request):
 
 
 def todo_create(request):
-    
-# Create your views here.
+    todolist = ToDoList.objects.all()
+    if request.method == 'POST':
+        title = request.POST['title']
+        new_todo = ToDoList(title=title, content="전송 잘됨", created_date=timezone.now())
+        new_todo.save()
+    return redirect('todo_list')
+
+
+def todo_delete(request, pk):
+    todo = get_object_or_404(ToDoList, pk=pk)
+    todo.delete()
+    return redirect('todo_list')
